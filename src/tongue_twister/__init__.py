680a6ccf2eca09514b386b56e9f8e569aa5414a7
sof202 <s.o.fletcher@exeter.ac.uk>
2025-07-05 19:27:58 +0100

    revert: remove old way of getting data

--- src/tongue_twister/__init__.py
+++
@@ -1,8 +0,0 @@
-import importlib.resources
-
-
-def load_tongue_twisters():
-    with importlib.resources.open_text(
-        "tongue_twister.data", "tongue_twisters.txt"
-    ) as tongue_twisters:
-        return tongue_twisters.read()
